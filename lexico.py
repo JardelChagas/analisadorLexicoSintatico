49ae8fcc202e4201304bcb14fbf58d3320e8a213
jardel <jardelchags@gmail.com>
2021-03-24 23:43:32 -0300

    type ok

--- lexico.py
+++ lexico.py
@@ -38,8 +38,10 @@ class Lexico:
 
     def structure(self):
         list = self.mainClass()
+        print(list)
         if list != "":
             self.classDeclaration(list[0], list[1])
+        print(self.out)
 
     def mainClass(self):
         mc = ["class", "Identifier", "{", "public", "static", "void", "main", "(", "String", "[", "]", "Identifier", ")",
@@ -51,62 +53,54 @@ class Lexico:
         for line in self.file:
             countLine += 1
             i = 0
-            while i < len(line):
-                if line[i] != " " and line[i] != "" and line[i] != "\n":
-                    token = token + line[i]
-                    #print(token)
-                    if token == mc[est] or mc[est] == "Identifier" or mc[est] == "Statement":
-                        if mc[est] == "Identifier":
-                            l = self.identifier(line, i)
-                            if l == "":
-                                print("erro line ", countLine)
-                                return ""
-                            else:
-                                self.countIdentifier += 1
-                                self.createRow(l[0], countLine)
-                                self.symbolTable = self.symbolTable.append(
-                                    {"value": l[0], "number": self.countIdentifier}, ignore_index=True)
+            if est < len(mc):
+                while i < len(line):
+                    if line[i] != " " and line[i] != "" and line[i] != "\n":
+                        token = token + line[i]
+                        #print(token)
+                        if token == mc[est] or mc[est] == "Identifier" or mc[est] == "Statement":
+                            if mc[est] == "Identifier":
+                                l = self.identifier(line, i)
+                                if l == "":
+                                    print("erro line ", countLine)
+                                    return ""
+                                else:
+                                    self.countIdentifier += 1
+                                    self.createRow(l[0], countLine)
+                                    self.symbolTable = self.symbolTable.append(
+                                        {"value": l[0], "number": self.countIdentifier}, ignore_index=True)
 
-                                i = l[2] - 1
+                                    i = l[2] - 1
+                                    token = ""
+                                est += 1
+                            elif mc[est] == "Statement":
+                                self.statement(line, countLine)
+                                est += 1
+                                i = len(line)
                                 token = ""
-                            est += 1
-                        elif mc[est] == "Statement":
-                            self.statement(line, countLine)
-                            est += 1
-                            i = len(line)
-                            token = ""
-                        elif mc[est] in self.symbols:
-                            for j in range(len(self.symbols)):
-                                if mc[est] == self.symbols[j]:
-                                    if j % 2 == 0:
-                                        self.createRow(token, countLine)
-                                        self.stack.append("#")
-                                        break
-                                    else:
-                                        self.createRow(token, countLine)
-                                        self.stack.pop()
-                                        break
-                            est += 1
-                            token = ""
+                            elif mc[est] in self.symbols:
+                                for j in range(len(self.symbols)):
+                                    if mc[est] == self.symbols[j]:
+                                        if j % 2 == 0:
+                                            self.createRow(token, countLine)
+                                            self.stack.append("#")
+                                            break
+                                        else:
+                                            self.createRow(token, countLine)
+                                            self.stack.pop()
+                                            break
+                                est += 1
+                                token = ""
+                            else:
+                                self.createRow(token, countLine)
+                                token = ""
+                                est += 1
+
                         else:
-                            self.createRow(token, countLine)
-                            token = ""
-                            est += 1
-                    else:
-                        if self.isErr(token, mc, countLine):
-                            print("erro! Simbolo invalido na linha ", countLine)
-                            return ''
-
-
-
-
-                i += 1
-        print(est)
-
-
-        print(self.out)
-        self.type("int 10", 0, countLine)
-
+                            if self.isErr(token, mc, countLine):
+                                print("erro! Simbolo invalido na linha ", countLine)
+                                return ''
+                    i += 1
         return [line, i]
 
     def classDeclaration(self, r, e):
@@ -205,10 +199,12 @@ class Lexico:
         print(l)
         list = ["int", "boolean", "int[]", "int["]
         if l[0] in list:
-           if (l[0] == "int" and l[1] == "[" and l[2] =="]") or (l[0] == "int[" and l[1] == "]"):
-               print("")
+           if (l[0] == "int" and l[1] == "[" and l[2] == "]") or (l[0] == "int[" and l[1] == "]") or (l[0] == "int" and l[1] == "[]") or l[0] == "int[]":
+               self.createRow("int", countLine)
+               self.createRow("[", countLine)
+               self.createRow("]", countLine)
            else:
-               print("")
+               self.createRow(l[0], countLine)
         else:
             l = self.identifier(line, i)
             if l == "":
@@ -220,8 +216,6 @@ class Lexico:
                 self.symbolTable = self.symbolTable.append(
                     {"value": l[0], "number": self.countIdentifier}, ignore_index=True)
 
-                i = l[2] - 1
-        token = ""
         '''
         for r in line:
             if r != " " and r != "" and r != "\n":
